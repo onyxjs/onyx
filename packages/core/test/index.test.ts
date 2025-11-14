72e14b1c545b7c4cece5360321133aa10ae64b59
Elijah Kotyluk <elijah@elijahkotyluk.com>
2025-11-14 05:44:09 -0600

    chore(Vitest): setup vitest unit testing.

---
+++ packages/core/test/index.test.ts
@@ -0,0 +1,5 @@
+import { expect, test } from 'vitest'
+
+test('3 to equal 3', () => {
+  expect(3).toBe(3)
+})
---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from 'vitest/config'
+
+export default defineConfig({
+  test: {
+    projects: [
+      'packages/*',
+    ],
+    reporters: ['json'],
+    coverage: {
+        provider: 'v8',
+    }
+  },
+})
